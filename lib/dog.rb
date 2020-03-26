8d981bfa0d98bb4c84981974b4753d28be44ff7a
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-03-26 06:38:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -41,8 +41,7 @@ class Dog
   end
 
   def self.new_from_db(row)
-    attributes = {id: row[0], name: row[1], breed: row[2]}
-    self.new(attributes)
+    self.new(id: row[0], name: row[1], breed: row[2])
   end
 
   def self.find_by_id(id)
